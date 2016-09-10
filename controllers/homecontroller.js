ec9cd672678b14c04d9bd94c8d4790841e880635
Eliezer Wohl <eliezerwohl@gmail.com>
2016-09-10 10:53:16 -0400

    added sequelize

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -1,3 +1,55 @@
+var models = require("../models/models.js");
+
 exports.homeController = function(req, res){
 	res.send("it worked!");
 }
+
+exports.register = function (req, res){
+  models.User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(function(results) {
+    if (results) {
+      res.redirect("/register?msg=Your email is already registered, please login.");
+    } else {
+      models.User.create({
+        lastname: (req.body.lastname).trim(),
+        firstname:  (req.body.firstname).trim(),
+        email: req.body.email,
+        // password: saltyhash(req.body.password),
+        password: req.body.password,
+      }).then(function() {
+        res.redirect("/?msg=Thanks for registering, please login.");
+      });
+    }
+  })
+}
+//  models.Organization.create({
+//     name: req.body.name
+//   }).then(function(data) { 
+//     console.log(data)
+//     var orgId = data.dataValues.id
+//   models.User.findOne({
+//     where: {
+//       email: req.body.email
+//     }
+//   }).then(function(results) {
+//     if (results) {
+//       res.redirect("/register?msg=Your email is already registered, please login.");
+//     } else {
+//       models.User.create({
+//         lastname: (req.body.lastname).trim(),
+//         firstname:  (req.body.firstname).trim(),
+//         email: req.body.email,
+//         password: saltyhash(req.body.password),
+//         phone:req.body.phone,
+//         OrganizationId:orgId,
+//         admin: true
+//       }).then(function() {
+//         res.redirect("/?msg=Thanks for registering, please login.");
+//       });
+//     }
+//   })
+// });
+// })
--- models/models.js
+++ models/models.js
@@ -6,46 +6,45 @@ if (process.env.NODE_ENV === 'production') {
   var connection = new Sequelize(process.env.JAWSDB_URL);
 } else {
   // LOCAL DB
-  var connection = new Sequelize('mmdb', 'root');
+  var connection = new Sequelize('match', 'root');
 }
-
-var BulkQuestion = connection.define("BulkQuestion", {
-question:Sequelize.STRING
-});
-
-
-var Note = connection.define("Note", {
-  note:Sequelize.STRING,
-  firstname:Sequelize.STRING,
-  lastname:Sequelize.STRING
-});
-
-var Patient = connection.define('Patient', {
-  firstname: Sequelize.STRING,
-  lastname: Sequelize.STRING,
-  dob:Sequelize.STRING,
-  phone:Sequelize.STRING,
-  email:Sequelize.STRING,
-  address1:Sequelize.STRING,
-  address2:Sequelize.STRING
-});
+// var BulkQuestion = connection.define("BulkQuestion", {
+// question:Sequelize.STRING
+// });
 
 
-var PatientComment = connection.define('Question', {
-  firstname: Sequelize.STRING,
-  lastname: Sequelize.STRING,
-  comment:Sequelize.STRING
-});
+// var Note = connection.define("Note", {
+//   note:Sequelize.STRING,
+//   firstname:Sequelize.STRING,
+//   lastname:Sequelize.STRING
+// });
 
-var Question = connection.define('Question', {
-  question: Sequelize.STRING,
-  answer: Sequelize.STRING
-});
+// var Patient = connection.define('Patient', {
+//   firstname: Sequelize.STRING,
+//   lastname: Sequelize.STRING,
+//   dob:Sequelize.STRING,
+//   phone:Sequelize.STRING,
+//   email:Sequelize.STRING,
+//   address1:Sequelize.STRING,
+//   address2:Sequelize.STRING
+// });
 
-var Organization = connection.define('Organization', {
-  name: Sequelize.STRING,
+
+// var PatientComment = connection.define('Question', {
+//   firstname: Sequelize.STRING,
+//   lastname: Sequelize.STRING,
+//   comment:Sequelize.STRING
+// });
+
+// var Question = connection.define('Question', {
+//   question: Sequelize.STRING,
+//   answer: Sequelize.STRING
+// });
+
+// var Organization = connection.define('Organization', {
+//   name: Sequelize.STRING,
  
-});
+// });
 
 var User = connection.define('User', {
   email: {
@@ -55,45 +54,43 @@ var User = connection.define('User', {
   password: Sequelize.STRING,
   firstname: Sequelize.STRING,
   lastname: Sequelize.STRING,
-  phone:Sequelize.STRING,
-  admin:{type:Sequelize.BOOLEAN, default:false}
-
 });
-Organization.hasMany(User);
-User.belongsTo(Organization);
 
-Organization.hasMany(Patient);
-Patient.belongsTo(Organization);
-User.hasMany(Patient);
-Patient.belongsTo(User);
+// Organization.hasMany(User);
+// User.belongsTo(Organization);
 
-User.hasMany(PatientComment);
-PatientComment.belongsTo(User);
-Organization.hasMany(PatientComment);
-PatientComment.belongsTo(Organization);
-Patient.hasMany(PatientComment);
-PatientComment.belongsTo(Patient);
+// Organization.hasMany(Patient);
+// Patient.belongsTo(Organization);
+// User.hasMany(Patient);
+// Patient.belongsTo(User);
 
-Patient.hasMany(Question);
-Question.belongsTo(Patient);
-User.hasMany(Question);
-Question.belongsTo(User);
-Organization.hasMany(Question);
-Question.belongsTo(Organization);
+// User.hasMany(PatientComment);
+// PatientComment.belongsTo(User);
+// Organization.hasMany(PatientComment);
+// PatientComment.belongsTo(Organization);
+// Patient.hasMany(PatientComment);
+// PatientComment.belongsTo(Patient);
 
-User.hasMany(Note);
-Note.belongsTo(User);
-Question.hasMany(Note);
-Note.belongsTo(Question);
-Organization.hasMany(Note);
-Note.belongsTo(Organization);
+// Patient.hasMany(Question);
+// Question.belongsTo(Patient);
+// User.hasMany(Question);
+// Question.belongsTo(User);
+// Organization.hasMany(Question);
+// Question.belongsTo(Organization);
+
+// User.hasMany(Note);
+// Note.belongsTo(User);
+// Question.hasMany(Note);
+// Note.belongsTo(Question);
+// Organization.hasMany(Note);
+// Note.belongsTo(Organization);
 
 
-connection.sync();
-exports.Organization=Organization;
+// connection.sync();
+// exports.Organization=Organization;
 exports.User=User;
-exports.Question=Question;
-exports.PatientComment=PatientComment;
-exports.Note=Note;
-exports.Patient=Patient;
-exports.BulkQuestion=BulkQuestion;
+// exports.Question=Question;
+// exports.PatientComment=PatientComment;
+// exports.Note=Note;
+// exports.Patient=Patient;
+// exports.BulkQuestion=BulkQuestion;
