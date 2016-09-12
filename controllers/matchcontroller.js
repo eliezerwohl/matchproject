1300e58fc04d666adcb350945182680880a990c4
eliezerwohl <eliezerwohl@gmail.com>
2016-09-12 15:30:11 -0400

    added my questions route

--- controllers/matchcontroller.js
+++ controllers/matchcontroller.js
@@ -1,1 +1,26 @@
 var models = require("../models/models.js");
+
+exports.myQuestions = function(req,res){
+
+
+models.Answer.update(
+  {
+    city: req.body.city,
+  	upper:req.body.upper
+  	lower:req.body.lower
+  	seeking:req.body.seeking,
+  	gender:req.body.gender
+  },
+  {
+    where: { UserId : req.user.id }
+  })
+  .then(function (result) { 
+  	debugger
+  	res.send("good")
+
+  }, function(rejectedPromiseError){
+  	debugger
+  	res.send("bad")
+  });
+
+}
