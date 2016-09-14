80932532648017904ae4890b5b86531e6b26d3ad
Eliezer Wohl <eliezerwohl@gmail.com>
2016-09-14 16:39:55 -0400

    finished next prime.  user will be able to get prime if they don't like thier

--- public/javascript/maker.js
+++ public/javascript/maker.js
@@ -1,6 +1,14 @@
 
-$("button").on("click", function(){
+$(".find").on("click", function(){
 	 $.ajax({url: "/findPrime", success: function(result){
+	 	console.log("good")
+	 }});
+})
+
+$(".next").on("click", function(){
+		 $.ajax({url: "/nextPrime", success: function(result){
 
 	 }});
+
+
 })
