205eca25ac15fbd8793d291abbe8b3c860b9a98b
eliezerwohl <eliezerwohl@gmail.com>
2016-09-21 17:13:29 -0400

    added psuedo code for storing matches

--- controllers/saveController.js
+++ controllers/saveController.js
@@ -2,5 +2,11 @@ var models = require("../models/models.js");
 
 exports.saveMatch = function(req, res){
 	res.send("got it");
+	first check the matched
 
+	if nothing
+		then put it in
+	else
+		find the vote of the two
+		add them in
 }
