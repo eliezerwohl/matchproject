0d9a7183a49d352a95d2fe8d52c15491e2860be0
Eliezer Wohl <eliezerwohl@gmail.com>
2016-09-23 14:54:47 -0400

    find all matches of the person logged in

--- controllers/userMatchController.js
+++ controllers/userMatchController.js
@@ -1,4 +1,16 @@
+var models = require("../models/models.js");
+
 
 exports.userMatch = function(req, res){
-	res.send("yo");
+	models.Matched.findAll({
+		where: {
+		
+    $or: {
+    		user1:2,
+     user2: 2,
+    }
+  }
+	}).then(function(data){
+		debugger
+	})
 }
