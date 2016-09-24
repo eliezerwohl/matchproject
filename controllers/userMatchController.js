edcd3ec9c2a493546ad0e23b22c8c97321b5b356
Eliezer Wohl <eliezerwohl@gmail.com>
2016-09-24 17:59:07 -0400

    updated models and way usermatch usearch to prevent user from seeing people more than once

--- controllers/userMatchController.js
+++ controllers/userMatchController.js
@@ -4,9 +4,12 @@ var findMatch = [];
 exports.userMatch = function(req, res){
 	models.Matched.findAll({
 		where: {
-		
+			//unless both ppl answer this should stay as OK
+		search:"OK",
+		//if the user responds, this will prevent this match from coming up again
+		answered:{$notIn[req.session.UserId]},
     $or: {
-    		user1:req.session.UserId,
+    user1:req.session.UserId,
      user2: req.session.UserId,
     }
   }
