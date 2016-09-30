df5d3d44a75af4e673cdbefd92c1110d0a3fc6b0
Eliezer Wohl <eliezerwohl@gmail.com>
2016-09-30 11:02:53 -0400

    added dailymatch count to user model, cleaned up and doubled the code that will handle the user voting.  no errors so far, but about to try the second person voting on the same match

--- controllers/userSaveController.js
+++ controllers/userSaveController.js
@@ -1,91 +1,126 @@
 var models = require("../models/models.js");
-exports.userSave = function(req, res){
-	debugger
-	if (req.session.dailyMatch < req.session.UserId){
-		models.Matched.update({
-			user1Vote:req.body.data
-			},
-			{where:{user1:req.session.dailyMatch, user2:req.session.UserId}
-		}).then(function(data, option){
-			debugger
-					models.Matched.findOne({
-						where:{user1:req.session.dailyMatch, user2:req.session.UserId}
-					}).then(function(data){
-						//grab the user 2 datat here
-						//grab the id of the match here
-						var voteId
-						if (data.dataValues.user1Vote === null ){
-								models.Matched.update({
-									answered:req.session.UserId
-								},
-								{
-								where:{id:data.dataValues.id}
-								})
-						}
-						else{
-							models.Matched.update({
-									search:"None"
-								},
-								{
-								where:{id:data.dataValues.id}
-								}).then(function(data){
-									models.Vote.findAll({
-										where:{
-											id:voteId
-										}
-									}).then(function(data){
-										if (user1.vote === user.vote){
-											//update scores correcly
-										}
-										else{
-											nothing
-										}
-										//update all their scores 
-									})
-								})
+var user1;
+var matchedId;
 
+exports.userSave = function(req, res) {
+  if (req.session.dailyMatch < req.session.UserId) {
+    models.Matched.update({
+      user1Vote: req.body.data
+    }, {
+      where: {
+        user1: req.session.dailyMatch,
+        user2: req.session.UserId
+    	}
+    }).then(function(data, option) {
+      models.Matched.findOne({
+        where: {
+            user1: req.session.dailyMatch,
+            user2: req.session.UserId
+        }
+      }).then(function(data) {
+      	matchedId	= data.dataValues.id
+	      if (data.dataValues.user1Vote === null) {
+	        //both haven't voted
+	        models.Matched.update({
+	          answered: req.session.UserId
+	        }, {
+            where: {
+            	id: data.dataValues.id
+            }
+	        })
+	      } else {
+    		 //both have voted
+          models.Matched.update({
+            search: "None"
+          }, {
+            where: {
+                id: matchedId
+            }
+          }).then(function(data) {
+            models.Vote.findAll({
+              where: {
+                MatchedId: matchedId
+              }
+            }).then(function(data) {
+              if (user1.vote === user2.vote) {
+                  //update scores correcly
+                  // get all the votes
+                  // two array, yes and not
+	              if (user.vote == 0) {
+	                  // update all with 0 +5
+	                  // update as a 1 as - 1
+                } else {
+                    // update all with 1 +5
+                    // update as a 0 as - 1
+                }
+            	} else {
+                // nothing has happened
+            	}
+        		});
+	        });
+        }
+      });
+    });
+  } else {
+    models.Matched.update({
+      user2Vote: req.body.data
+    }, {
+      where: {
+        user2: req.session.dailyMatch,
+        user1: req.session.UserId
+      }
+    }).then(function(data, option) {
+      models.Matched.findOne({
+        where: {
+          user2: req.session.dailyMatch,
+          user1: req.session.UserId
+        }
+      }).then(function(data) {
+        matchedId	= data.dataValues.id
+        if (data.dataValues.user2Vote === null) {
+            //both haven't voted
+          models.Matched.update({
+              answered: req.session.UserId
+          }, {
+              where: {
+                  id: matchedId
+              }
+          })
+        } 
+        else {
+              //both have voted
+          models.Matched.update({
+            search: "None"
+          }, {
+            where: {
+                MatchedId: matchedId
+            }
+          }).then(function(data) {
+            models.Vote.findAll({
+                where: {
+                    id: voteId
+                }
+            }).then(function(data) {
+              if (user1.vote === user2.vote) {
+                  //update scores correcly
+                  // get all the votes
+                  // two array, yes and not
+                if (user.vote == 0) {
+                    // update all with 0 +5
+                    // update as a 1 as - 1
 
-						}
-					})
-		})
-	}
-	else {
-		models.Matched.update({
-			user2Vote:req.body.data
-			},
-			{where:{user2:req.session.dailyMatch, user1:req.session.UserId}
-		}).then(function(data, option){
-			debugger
-		})
-	}
-	console.log("hit it")
+                } else {
+                    // update all with 1 +5
+                    // update as a 0 as - 1
+                }
+            	} else {
+                // nothing has happened
+            	}
+        		});
+          });
+        }
+      });
+    });
+  }
 }
 
-
-
-if one equls null or other equals null
-	update  answered:req.session.UserId
-else{
-	update 
-}
-
-// when  i go to save
-
-// find the match again
-// if 3 !=0, 
-// 	put that user id there
-// 	see which id is larger
-// 	if user is larger, change user2vote to true or false
-// 		else
-// 	user 1
-
-// else {
-// 	if current useranswer = false{
-// 		change search to false
-// 		-1 for everyboyd
-// 	}
-// 	else if {, 
-// 		see if user1 vote = true
-// 		if it is, and currentUser also = true
-// 			true match()
-// }
