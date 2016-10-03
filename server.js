d9018a543dd0ce4d781f6826e99f213cc80d4428
Eliezer Wohl <eliezerwohl@gmail.com>
2016-10-03 13:51:00 -0400

    can console.log message

--- server.js
+++ server.js
@@ -34,4 +34,14 @@ var server = app.listen(PORT, function(){
 var io = require('socket.io').listen(server);
 io.on('connection', function(socket){
   console.log('a user connected');
+
+    socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+     socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
+
+
+
