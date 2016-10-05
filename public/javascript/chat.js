50b5e15683abafa6d83e18ce2a036c48d91d05e9
Eliezer Wohl <eliezerwohl@gmail.com>
2016-10-05 18:01:49 -0400

    fixed bug where matches if both users were saying yes weren't saving properly

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,17 +1,11 @@
 $( document ).ready(function() {
   var socket = io();
-	// $.ajax({url: "/chatroom", success: function(result){
-	//  debugger
-     socket.emit('room', "data");
-	// }});	
-
-
+  socket.emit('room', "data");
   $('form').submit(function(){
     socket.emit('chat message', $('#m').val());
     $('#m').val('');
     return false;
   });
-
   socket.on('chat message', function(msg){
       $('#messages').append($('<li>').text(msg));
     });
