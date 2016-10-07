a52b028daaa4854b508f20c50ce96372b8a4a0b7
Eliezer Wohl <eliezerwohl@gmail.com>
2016-10-07 17:56:12 -0400

    added custom function that get the values of the inputs, checks to make sure all inputs are filled, and if not, circles them in red

--- public/javascript/myquestions.js
+++ public/javascript/myquestions.js
@@ -1,20 +1,37 @@
 $( document ).ready(function() {
+	var infoArray = ["seeking", "age", "city", "gender", "upper", "lower"]
+	var data;
 	$("#myInfo").on("click", function(){
-		var data = {
-			seeking:$("#seeking").val(),
-			age:$("#age").val(),
-			city:$("#city").val(),
-			gender:$("#gender").val(),
-			upper:$("#upper").val(),
-			lower:$ ("#lower").val()
-		}
-		$.ajax({url: "/myInfoUpdate", type:"POST", data:data, success: function(result){
-			$("#myModal").modal("hide");
-			if ($("#a091202").val() == ' '){
-				$("#myModal2").modal("show")
+		var counter = 0
+		var error = []
+		data = {}
+
+		for (var i = 0; i < infoArray.length; i++) {
+			counter++
+			data[infoArray[i]] = $("#" + infoArray[i]).val();
+			debugger
+			if (data[infoArray[i]] == null || data[infoArray[i]].length < 1 ){
+				error.push(i);
 			}
-		}});
-	});
+			if (counter == infoArray.length ){
+				if (error.length < 1){
+					$.ajax({url: "/myInfoUpdate", type:"POST", data:data, success: function(result){
+					$("#myModal").modal("hide");
+					//will auto bring up the questions if the user hasn't filled them out
+					if ($("#a091202").val() == ' '){
+						$("#myModal2").modal("show")
+					}
+				}});
+				}
+				else{
+					for (var i = 0; i < error.length; i++) {
+						$("#" + infoArray[error[i]]).addClass("error");
+					}
+				}
+			}
+		}
+
+	})
 
 
 	$.ajax({url: "/myInfo", success: function(result){
