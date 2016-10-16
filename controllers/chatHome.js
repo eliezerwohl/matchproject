7fa577a5073612c1f85832ca65ad73e052ff857f
Eliezer Wohl <eliezerwohl@gmail.com>
2016-10-16 19:17:28 -0400

    hk

---
+++ controllers/chatHome.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+	  var socket = io();
+	$.ajax({url: "/findChat", success: function(result){
+		for (var i = 0; i < result.length; i++) {
+		 $(".target").append("<button class='" + result[i].checked 
+		 	+ " chat col-xs-12 btn btn-default' value=" 
+		 	+ result[i].arrayId + "><h4 class='pull-left'>" + result[i].firstname + " "
+		 	+result[i].lastname + "</h4><br><span id='" + result[i].updateId + "'>"
+		 	+ result[i].msg + "</span></button>")
+		}
+	}});
+	$(".target").on("click", ".chat", function(){
+		var data = this.value;
+		$.ajax({url: "/chatId", type:"POST", data:{data:data},  success: function(result){
+			 window.location = "/chat"
+	}})
+	})
+});
