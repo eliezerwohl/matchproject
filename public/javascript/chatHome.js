4242de2765cbd30109bbc08a939055587f83c735
Eliezer Wohl <eliezerwohl@gmail.com>
2016-10-18 13:30:46 -0400

    fixed chat, can now enter when pressing enter, messages are now color coded, if user send last one it's blue, if they user hasn't check thier llast message it's dark, if they have it's lighter

--- public/javascript/chatHome.js
+++ public/javascript/chatHome.js
@@ -3,10 +3,11 @@ $( document ).ready(function() {
 	$.ajax({url: "/findChat", success: function(result){
 		debugger
 		for (var i = 0; i < result.length; i++) {
-		  $(".target").append("<button class='" + result[i].updateId  + " " + result[i].checked +  result[i].user
-		 	+ " chat col-xs-12 btn btn-default' value=" 
+		  $(".target").append("<button class='chat col-xs-12 btn btn-default' value=" 
 		 	+ result[i].arrayId + "><h4 class='pull-left'>" + result[i].firstname + " "
-		 	+result[i].lastname + "</h4><br><span id='" + result[i].updateId + "'>"
+		 	+result[i].lastname + ":</h4><br><span id='" + result[i].updateId + "' class='pull-left " + 
+		 	 result[i].checked +  result[i].user
+		 	+"'>"
 		 	+ result[i].msg + "</span></button>")
 		}
 	}});
--- public/javascript/notify.js
+++
@@ -1,4 +0,0 @@
-// $( document ).ready(function() { 
-// 	socket = io()
-// 	socket.emit("notify")
-// 	});
