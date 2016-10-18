29176e98f3400012f7431ae383b533b289f42b1d
Eliezer Wohl <eliezerwohl@gmail.com>
2016-10-18 15:31:03 -0400

    hk fix

---
+++ public/javascript/myQuestions.js
@@ -0,0 +1,54 @@
+$( document ).ready(function() {
+	var infoArray = ["seeking", "age", "city", "gender", "upper", "lower"]
+	var questionArray = ["a091201", "a091202", "a091203", "a091204"]
+//on first login, modal "welcome, would you like to setup your connection profile?  It'll only take a few minutes
+//or if your just here to help connect others, click here
+
+	$("#myInfo").on("click", function(){
+		myProfile(infoArray, "/myInfoUpdate")
+	})
+
+	$("#myQuestions").on("click", function(){
+		myProfile(questionArray, "/myQuestionsUpdate")
+	});
+
+	function myProfile(array, url){
+		var counter = 0
+		var error = []
+		var data = {}
+		for (var i = 0; i < array.length; i++) {
+			counter++
+			data[array[i]] = $("#" + array[i]).val();
+			debugger
+			if (data[array[i]] == null || data[array[i]].length < 1 ){
+				error.push(i);
+			}
+			if (counter == array.length ){
+				if (error.length < 1){
+					$.ajax({url: url, type:"POST", data:data, success: function(result){
+						if (result == "greeting"){
+							window.location.pathname = "/myQuestions"
+						}
+						else {
+							window.location.pathname = "/myProfile"
+						}
+						
+					}});
+				}
+				else{
+					for (var i = 0; i < error.length; i++) {
+						$("#" + array[error[i]]).addClass("error");
+					}
+				}
+			}
+		}
+	}
+	$.ajax({url: "/myInfoData", success: function(result){
+		if (result === "blank"){
+			return false
+		}
+		else {
+			append(result, "myQuestions")
+		}
+	}});
+});
