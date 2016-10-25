b02a58d968f97527acb1bd76156fbf9dee4b4739
Eliezer Wohl <eliezerwohl@gmail.com>
2016-10-24 20:33:02 -0400

    added form validation to login form

--- public/javascript/functions.js
+++ public/javascript/functions.js
@@ -7,40 +7,38 @@ function append(data, role){
 	});
 }
 
-function formCheck(url){
-	// var array = []
+function formCheck(url, login){
+	//used 4 different times across 4 different pages
 	var input = jQuery.makeArray(document.getElementsByTagName("input"));
 	var textarea = jQuery.makeArray(document.getElementsByTagName("textarea"))
 	var select = jQuery.makeArray(document.getElementsByTagName("select"))
 	var elements = input.concat(select, textarea);
 	var array = jQuery.makeArray(elements)
-	var counter = 0
-		var error = []
-		var data = {}
+	var counter = 0;var error = [];var data = {};
 	for (var i = 0; i < array.length; i++) {
-		// array.push(unsortedArray[i].id)
-		counter++
-			data[array[i].id] = $("#" + array[i].id).val();
-			if (data[array[i].id] == null || data[array[i].id].length < 1 ){error.push(i);}
-			if (counter == array.length ){
-				if (error.length < 1){
+		counter++;data[array[i].id] = $("#" + array[i].id).val();
+		if (data[array[i].id] == null || data[array[i].id].length < 1 ){error.push(i);}
+		if (counter == array.length ){
+			if (error.length < 1){
+				if (login) {return true}
+				else {
 					$.ajax({url: url, type:"POST", data:data, success: function(result){
 						//make into a switch case
 						if (result == "greeting"){window.location.pathname = "/myQuestions";}
 						else if (result == "myInfo" || result == "myQuestions") {window.location.pathname = "/myProfile";}
 						else if (result == "taken"){$(".errorMsg").show();}
 						else if (result == "accept"){
-							document.cookie = "signup=true"
+							document.cookie = "signup=true";
 							window.location.pathname="/";
 						}
 					}});
 				}
-				else{
-					for (var i = 0; i < error.length; i++) {
-						$("#" + array[error[i]].id).addClass("error");
-					}
+			}
+			else {
+				for (var i = 0; i < error.length; i++) {
+					$("#" + array[error[i]].id).addClass("error");
 				}
 			}
-	}
-
+		}
+	} 
 }
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,31 +2,22 @@ $( document ).ready(function() {
 	function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
+      for(var i = 0; i <ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
-        }
+        while (c.charAt(0)==' ') {c = c.substring(1);}
+        if (c.indexOf(name) == 0) {return c.substring(name.length,c.length);}
     	}
     	return "";
 		}
  		if (getCookie("signup") == "true"){
- 			$(".thanks").show()
- 			document.cookie = "signup=; expires=Thu, 01 Jan 1970 00:00:00 UTC"		
+ 			$(".thanks").show();
+ 			document.cookie = "signup=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 		}
-		if (window.location.search == "?incorrect") {
-			$(".incorrect").show()
-		}
-		$("#loginButton").on("click", function(){
-			formCheck("/login")
-		})
-	
-
-
-
-
-
+		if (window.location.search == "?incorrect") { $(".incorrect").show();}
+    $("#loginButton" ).on("click", function(){ formCheck("/login");});
+		$( "#loginForm" ).submit(function( event ) {
+      var form = formCheck(undefined, true)
+      if (form == true) {return}
+      event.preventDefault();
+    });
 });
