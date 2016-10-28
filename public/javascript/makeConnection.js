4848bd6ce4d0e4d84419ca0d7203d813c42bb5b0
Eliezer Wohl <eliezerwohl@gmail.com>
2016-10-28 18:46:50 -0400

    cleaned up code further, got rid of old model sql dumps

--- public/javascript/makeConnection.js
+++ public/javascript/makeConnection.js
@@ -1,67 +1,41 @@
 // //make this load on page load
 $( document ).ready(function() {
 $("#closeModal").on("click", function(){
-	$("#warningModal").modal("hide");
-	getPrime();
-})
+	$("#warningModal").modal("hide");getPrime();
+});
 	//fixes sensitivity issue on mobile devices
 $.event.special.swipe.scrollSupressionThreshold = (screen.availWidth) / 60;
 $.event.special.swipe.horizontalDistanceThreshold = (screen.availWidth) / 60;
 $.event.special.swipe.verticalDistanceThreshold = (screen.availHeight) / 13;
-	function chatSize (){
-    var height = ($(window).height()); 
-    $("#chatBox").css("height", height-117 + "px")
-  }
-  chatSize()
-  $(window).resize(chatSize);
+	function chatSize (){var height = ($(window).height()); $("#chatBox").css("height", height-117 + "px");}
+  chatSize();$(window).resize(chatSize);
 	function getPrime(){
 		$("#rightText").text("next");
 		$(".pull-left").show();
 		$(".pull-right").hide();
 		$(".main, #buttonControl").off("swiperight");
 		$(".main, #buttonControl").off("swipeleft");
-
-		//control
 		$(".save").off("click");
 		$(".toggle").css("visibility", "hidden");
-		$("#left").on("click", function(){
-			next();
-		});
-		$("#right").on("click", function(){
-			getMatch();
-		});
-		$(".main, #buttonControl").on("swiperight",function(event){
-			event.stopImmediatePropagation();
-	  	getMatch();
-		});
-		$(".main, #buttonControl").on("swipeleft",function(event){
-			event.stopImmediatePropagation();
-	  	next();
-		});
-		$.ajax({url: "/findPrime", success: function(result){;
-		 	append(result, "prime");
-		}});
+		$("#left").on("click", function(){next();});
+		$("#right").on("click", function(){getMatch();});
+		$(".main, #buttonControl").on("swiperight",function(event){event.stopImmediatePropagation();getMatch();});
+		$(".main, #buttonControl").on("swipeleft",function(event){event.stopImmediatePropagation();next();});
+		$.ajax({url: "/findPrime", success: function(result){append(result, "prime");}});
 	}
 	getPrime();
 	function next(){
-			$.ajax({url: "/nextPrime", success: function(result){
-			if (result === false){
-				getPrime();
-			}
-			else{
-			 	append(result, "prime");
-			 }
+		$.ajax({url: "/nextPrime", success: function(result){
+			if (result === false){getPrime();}
+			else{append(result, "prime");}
 		}});
 	}
-
-//get match
 	function getMatch(){
 		$(".main, #buttonControl").off("swiperight");
 		$(".main, #buttonControl").off("swipeleft");
 		$(".main, #buttonControl").on("swiperight",function(event){event.stopImmediatePropagation();save(1)});
 		$(".main, #buttonControl").on("swipeleft",function(event){event.stopImmediatePropagation();save(0)});
-		$(".pull-left").hide();
-		$(".pull-right").show();
+		$(".pull-left").hide();$(".pull-right").show();
 		$("#left, #right").off("click");
 		$("#rightText").text("no");
 		$(".save").on("click", function(){
@@ -72,7 +46,7 @@ $.event.special.swipe.verticalDistanceThreshold = (screen.availHeight) / 13;
 			if (result === false){
 				//you've matched everybody
 				$(".main, #buttonControl").off("swiperight");
-		$(".main, #buttonControl").off("swipeleft");
+				$(".main, #buttonControl").off("swipeleft");
 				$("#warningModal").modal("show");
 			}
 			else{append(result, "match");$(".toggle").css("visibility", "visible");}
@@ -83,24 +57,12 @@ $.event.special.swipe.verticalDistanceThreshold = (screen.availHeight) / 13;
 		$.ajax({url: "/nextMatch", success: function(result){
 			if (result === false){
 				$(".main, #buttonControl").off("swiperight");
-		$(".main, #buttonControl").off("swipeleft");
+				$(".main, #buttonControl").off("swipeleft");
 				$("#warningModal").modal("show");
 			}
-			else{
-		 		append(result, "match");
-		 	}
+			else{append(result, "match");}
 		}});
 	}
-
-	function save(data){
-		$.ajax({url: "/saveMatch", type:"POST", data:{data:data}, success: function(result){
-			nextMatch();
-		}});
-	}
-
-	$(".toggle").on("click", function(){
-		$(".pull-left, .pull-right").toggle();
-	});
+	function save(data){$.ajax({url: "/saveMatch", type:"POST", data:{data:data}, success: function(result){nextMatch();}});}
+	$(".toggle").on("click", function(){$(".pull-left, .pull-right").toggle();});
 });
-
-
